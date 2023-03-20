1f6e2099aaa3317b34666a26c36b344111426552
PatrickRusso <113122527+PatrickRusso@users.noreply.github.com>
2023-03-20 13:34:16 -0400

    Home page design progress

--- Client/simple-steam/src/Components/Nav.jsx
+++ Client/simple-steam/src/Components/Nav.jsx
@@ -4,12 +4,14 @@ import { Link } from 'react-router-dom'
 
 export default function Nav(){
     return (
+        <div className='nav-container'>
 <nav className='navbar'>
-      <Link to="/">Home</Link>
-      <Link to="/profile">Profile</Link>
-      <Link to="/games">Games</Link>
-      <Link to="/wishlist">Wishlist</Link>
+      <Link className='links' to="/">Home</Link>
+      <Link className='links' to="/profile">Profile</Link>
+      <Link className='links' to="/games">Games</Link>
+      <Link className='links' to="/wishlist">Wishlist</Link>
     </nav>
+    </div>
 
     )
 }
