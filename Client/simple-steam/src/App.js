bb930fbdfaa404a69708e6ecf50f4f5c0ddd8c9c
Faatimah Cleveland <faatimahcleveland@gmail.com>
2023-03-20 10:13:53 -0400

    app.css added to app.js

--- Client/simple-steam/src/App.js
+++ Client/simple-steam/src/App.js
@@ -1,9 +1,10 @@
-// import { useState, useEffect } from 'react'
- import { Route, Routes } from 'react-router'
- import Nav from './Components/Nav'
- import Main from './Pages/Main'
+// import { useState, useEffect } from 'react';
+ import { Route, Routes } from 'react-router';
+ import Nav from './Components/Nav';
+ import Main from './Pages/Main';
 //  import Login from './Pages/Login'
- import Header from './Components/Header'
+ import Header from './Components/Header';
+ import './App.css'
 
 
 
