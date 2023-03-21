cd43a061ab83c7820e101f070ae89e283a8dd99e
PatrickRusso <113122527+PatrickRusso@users.noreply.github.com>
2023-03-21 12:11:03 -0400

    Cards for homepage made

--- Client/simple-steam/src/Pages/Main.js
+++ Client/simple-steam/src/Pages/Main.js
@@ -4,36 +4,29 @@ import { useNavigate } from "react-router-dom";
 import { GetGames } from "../services/GameServices";
 
 const Main = ({ user, authenticated }) => {
-    const [games, setGames] = useState([]);
-    let navigate = useNavigate();
+  const [games, setGames] = useState([]);
+  let navigate = useNavigate();
 
-    useEffect(() => {
-      const handleGames = async () => {
-        const data = await GetGames();
-        setGames(data);
-      };
-      handleGames();
-    }, []);
+  useEffect(() => {
+    const handleGames = async () => {
+      const data = await GetGames();
+      setGames(data);
+    };
+    handleGames();
+  }, []);
 
-    return (
-      <div className="main-container">
-        <Link to="/login">
-          <button className="login-button">Login</button>
-        </Link>
-        
-        <div className="grid-games">
-            <h1>Test</h1>
-            {games.map((game) => (
-              <div className="card" key={game.id}>
-                <h3>{game.title}</h3>
+  return (
+    <div className="main-container">
+      <div className="grid-games">
+        {games.map((game) => (
+          <div className="card" key={game.id}>
+            <h3>{game.title}</h3>
 
-                {console.log(game.title)}
-              </div>
-            ))}
+            {console.log(game.title)}
           </div>
-        </div>
-      
-      
-    );
-  };
-export default Main
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Main;
