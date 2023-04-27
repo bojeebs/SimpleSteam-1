f8d381f7116302b43493027d9ab770663eb95dbb
Tyler Carter <113203952+bojeebs@users.noreply.github.com>
2023-04-27 10:23:21 -0500

    psuh

--- BackendFolder/models/games.js
+++ BackendFolder/models/games.js
@@ -6,11 +6,7 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Games extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    
     static associate(models) {
       Games.belongsToMany(models.Wishlist, { through: 'Wishlist', foreignKey: 'id'})
     }
