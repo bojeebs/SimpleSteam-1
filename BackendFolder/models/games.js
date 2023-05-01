81861a52b13afb118b518f566ec373a341278fb6
Tyler Carter <113203952+bojeebs@users.noreply.github.com>
2023-05-01 11:17:49 -0500

    Update games.js

--- BackendFolder/models/games.js
+++ BackendFolder/models/games.js
@@ -8,18 +8,27 @@ module.exports = (sequelize, DataTypes) => {
   class Games extends Model {
     
     static associate(models) {
-      Games.belongsToMany(models.Wishlist, { through: 'Wishlist', foreignKey: 'id'})
+      Games.belongsToMany(models.Wishlist, { through: 'Wishlist', foreignKey: 'gameId'})
     }
   }
   Games.init({
     title: DataTypes.STRING,
     shortdescription: DataTypes.TEXT,
     image: DataTypes.STRING,
-    video: DataTypes.STRING
+    video: DataTypes.STRING,
+    createdAt: {
+      type: DataTypes.DATE,
+      field: 'createdat'
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      field: 'updatedat'
+    }
   }, {
     sequelize,
     modelName: 'Games',
-    tableName: 'games'
+    tableName: 'games',
+    underscored: true
   });
   return Games;
 };
